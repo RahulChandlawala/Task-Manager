b52ff55146964694632eeff66ca03430799d46fb
Rahul <rahulchandlawala@gmail.com>
2025-09-17 14:41:23 +0530

    Add notFound middleware for handling 404 errors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found");
 require("dotenv").config();
 
 // middlewares
@@ -14,6 +15,8 @@ app.use(express.json());
 // });
 
 app.use("/api/v1/tasks", tasks);
+
+app.use(notFound);
 
 const port = 3000;
 
